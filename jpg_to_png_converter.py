d72a32bc2b3c278584bd784c1334e72fe076fec1
Jack Caldwell-Nichols <jackcaldwell-nichols@Jacks-MacBook-Pro.local>
2024-05-19 23:16:47 +0200

    improved, cleaner

--- jpg_to_png_converter.py
+++ jpg_to_png_converter.py
@@ -3,29 +3,20 @@ import sys
 from PIL import Image
 import pathlib
 
-a = input("1: ")
-b = input("2: ")
+#a = input("1: ")
+#b = input("2: ")
 
 
-#a = sys.argv[1]
-#b = sys.argv[2]
+a = sys.argv[1]
+b = sys.argv[2]
 
 
-def jpg_to_png(a, b):
-    if os.path.exists(f'{b}/'):
-        for file in os.listdir(f'{a}/'):
-            filename = os.fsdecode(file)
-            new_image = Image.open(f'{a}/{filename}')
-            new_image.save(f'{b}/{filename.split(".")[0]}.png')
-            print(f"New file {filename} created and added to the directory '{b}'")
-    else:
-        p = pathlib.Path(f'{b}/')
-        p.mkdir(parents=True, exist_ok=True)
-        for file in os.listdir(f'./{a}'):
-            filename = os.fsdecode(file)
-            new_image = Image.open(f'{a}/{filename}')
-            new_image.save(f'{p}/{filename.split(".")[0]}.png', 'png')
-            print(f"New file created and added to the directory '{b}'")
+if not os.path.exists(b):
+    os.mkdir(b)
+
+for file in os.listdir(a):
+    new_img = Image.open(f'{a}/{file}')
+    new_img.save(f'{b}/{file.split(".")[0]}.png', 'png')
+    print(f"New image saved")
 
 
-jpg_to_png(a, b)
